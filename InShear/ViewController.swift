f0b693a0fb4353d27cca101b628d8a1ab64d0651
khay4icloud <khay2kalyan@icloud.com>
2017-08-08 16:11:13 -0500

    Intial commit

--- InShear/ViewController.swift
+++ InShear/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shearLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if #available(iOS 9.0, *) {
+                if traitCollection.forceTouchCapability == UIForceTouchCapability.available {
+                    if touch.force >= touch.maximumPossibleForce {
+                        shearLabel.text = "385+ grams"
+                    } else {
+                        let shear = touch.force/touch.maximumPossibleForce
+                        let grams = shear * 385
+                        let roundGrams = Int(grams)
+                        shearLabel.text = "\(roundGrams) grams"
+                    }
+                    
+                }
+            }
+        }
+    }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        shearLabel.text = "0 grams"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
